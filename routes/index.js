42587e1d1fd2ed79bd2dc1421ea5d3074e6cc012
Joseph Spira <spira.joe214@gmail.com>
2018-02-08 12:39:55 -0500

    Added config properties

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express'),
     exphbs = require('express-handlebars'),
     nodemailer = require('nodemailer'),
-    keys = require('../config/keys'),
+    // keys = require('../config/keys'),
     bodyParser = require('body-parser'),
     fetch = require('node-fetch'),
     router = express.Router();
@@ -13,7 +13,7 @@ let transporter = nodemailer.createTransport({
     port: 587,
     auth: {
         user: "joseph@oceanhomehealth.com",
-        pass: keys.emailPass
+        pass: 'Hellopo1i' //keys.emailPass
     },
     tls: {
         ciphers: 'SSLv3'
@@ -64,7 +64,7 @@ router.get('/getPriceTable', (req, res) => {
 router.post('/getGeoWeather', (req, res) => {
         let lat = req.body.latitude;
         let lon = req.body.longitude;
-        const APIKey = keys.weatherAPI;
+        const APIKey = '790e3bcb8a16e2395b51c9f39b7909f7'; //keys.weatherAPI;
         fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${APIKey}`)
             .then(response => response.json())
             .then(data => res.send(data))
