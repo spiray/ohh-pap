6d068587a710c6d25befafc41c3393b175e21c6c
Joseph Spira <spira.joe214@gmail.com>
2018-02-22 09:58:21 -0500

    Weather api

--- public/js/HomePage.js
+++ public/js/HomePage.js
@@ -271,7 +271,7 @@ const geoLocation = () => {
             .then(response => response.json())
             .then(data => {
                 locationDisplay.innerHTML = `${data.name} - ${round(data.main.temp * 9 / 5 - 459.67)} &#8457;
-                    <img width="26" height="26" src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" />`;
+                    <img width="26" height="26" src="${prodHost}http://openweathermap.org/img/w/${data.weather[0].icon}.png" />`;
             });
     });
 }
@@ -294,7 +294,7 @@ const getWeather = position => {
         .then(response => response.json())
         .then(data => {
             locationDisplay.innerHTML = `${data.name} - ${round(data.main.temp * 9 / 5 - 459.67)} &#8457;
-                                             <img width="26" height="26" src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" />`
+                                             <img width="26" height="26" src="${prodHost}http://openweathermap.org/img/w/${data.weather[0].icon}.png" />`
         })
         .catch(err => console.log(`API Error ${err}`));
 }
