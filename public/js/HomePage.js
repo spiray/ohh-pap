8886a96c71c8b8b6753ebd2343b7852a5792277f
Joseph Spira <spira.joe214@gmail.com>
2018-02-22 10:04:01 -0500

    Weather api

--- public/js/HomePage.js
+++ public/js/HomePage.js
@@ -14,20 +14,9 @@ let dateValue,
     resultLoc,
     resultCard,
     priceData;
+
 //  Declare and set constants
-const addToDate = select('#addToDate'),
-    dateSum = select('#dateSum'),
-    dateInput = select('#dateInput'),
-    dateInput2 = select('#dateInput2'),
-    dateInput3 = select('#dateInput3'),
-    dateInput4 = select('#dateInput4'),
-    findLoc = select('.findLoc'),
-    copyBtn = select('#copyToClip'),
-    locationDisplay = select('#locationDisplay'),
-    // getWeather = select('#getWeather'),
-    contactSubmit = select('#contact-submit'),
-    placeholderJson = select('#placeholder-json'),
-    prodHost = 'https://pure-escarpment-35043.herokuapp.com/',
+const prodHost = 'https://pure-escarpment-35043.herokuapp.com/',
     devHost = 'http://localhost:5000/';
 
 function preload() {
@@ -45,20 +34,17 @@ function preload() {
 
 function setup() {
     // //  Declare and set constants
-    // const addToDate = select('#addToDate'),
-    //     dateSum = select('#dateSum'),
-    //     dateInput = select('#dateInput'),
-    //     dateInput2 = select('#dateInput2'),
-    //     dateInput3 = select('#dateInput3'),
-    //     dateInput4 = select('#dateInput4'),
-    //     findLoc = select('.findLoc'),
-    //     copyBtn = select('#copyToClip'),
-    //     locationDisplay = select('#locationDisplay'),
-    //     getWeather = select('#getWeather'),
-    //     contactSubmit = select('#contact-submit'),
-    //     placeholderJson = select('#placeholder-json'),
-    //     prodHost = 'https://pure-escarpment-35043.herokuapp.com/',
-    //     devHost = 'http://localhost:5000/';
+    const addToDate = select('#addToDate'),
+        dateSum = select('#dateSum'),
+        dateInput = select('#dateInput'),
+        dateInput2 = select('#dateInput2'),
+        dateInput3 = select('#dateInput3'),
+        dateInput4 = select('#dateInput4'),
+        findLoc = select('.findLoc'),
+        copyBtn = select('#copyToClip'),
+        locationDisplay = select('#locationDisplay'),
+        // getWeather = select('#getWeather'),
+        contactSubmit = select('#contact-submit');
 
     //Set variables
     zipSearch = select('.search');
