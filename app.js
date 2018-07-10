b706aa003403cc6cb06fd8cffdf60d8fd6556858
Joseph Spira <spira.joe214@gmail.com>
2018-07-10 09:23:16 -0400

    added jsdoc

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-// Import modules and init app
+/** Import modules and init app */
 const express = require(`express`),
     exphbs = require(`express-handlebars`),
     path = require(`path`),
     bodyParser = require(`body-parser`);
 //keys = require('./config/keys');
 
-// Initialize App
+/** Initialize App */
 const app = express();
 
-// Load routes
+/** Load routes */
 const routes = require(`./routes/index`);
 
-// Confirgure views engine
+/** Confirgure views engine */
 /* global __dirname */
 app.set(`views`, `${__dirname}/views`);
 const hbs = exphbs.create({
@@ -21,19 +21,19 @@ const hbs = exphbs.create({
 app.engine(`handlebars`, hbs.engine);
 app.set(`view engine`, `handlebars`);
 
-// Deliver static files
+/** Deliver static files */
 app.use(express[`static`](path.join(__dirname, `/dist/public`)));
 
-// Configure Body parser to streamline HTTP requests
+/** Configure Body parser to streamline HTTP requests */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// Use routes
+/** Use routes */
 app.use(routes);
 
-// Set port and listen on that port
+/** Set port and listen on that port */
 /* global process */
 app.set(`port`, (process.env.PORT || 5000));
 app.listen(app.get(`port`), () => console.log(`App is running on port: ${app.get(`port`)}`));
--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,52 @@
-const gulp = require('gulp'),
-    imagemin = require('gulp-imagemin'),
-    cleanCSS = require('gulp-clean-css'),
-    minify = require('gulp-minify'),
-    sourcemaps = require('gulp-sourcemaps'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat');
+const gulp = require(`gulp`),
+    imagemin = require(`gulp-imagemin`),
+    cleanCSS = require(`gulp-clean-css`),
+    minify = require(`gulp-minify`),
+    rename = require(`gulp-rename`),
+    concat = require(`gulp-concat`);
 
 const paths = {
-    images: './public/images/*',
-    scripts: './public/js/*.js',
-    styles: './public/css/*.css',
-    data: './public/data/*.csv',
-    docs: './public/docs/*/*'
+    images: `./public/images/*`,
+    scripts: `./public/js/*.js`,
+    styles: `./public/css/*.css`,
+    data: `./public/data/*.csv`,
+    docs: `./public/docs/*/*`
 };
 
 const dests = {
-    images: 'dist/public/images',
-    scripts: 'dist/public/js',
-    styles: 'dist/public/css',
-    data: 'dist/public/data',
-    docs: 'dist/public/docs/'
+    images: `dist/public/images`,
+    scripts: `dist/public/js`,
+    styles: `dist/public/css`,
+    data: `dist/public/data`,
+    docs: `dist/public/docs/`
 };
 
-gulp.task('default', ['images', 'minify-css', 'minify-js', 'data', 'docs']);
+gulp.task(`default`, [`images`, `minify-css`, `minify-js`, `data`, `docs`]);
 
-gulp.task('images', () => {
-    return gulp.src(paths.images)
-        .pipe(imagemin({ optimizationLevel: 5 }))
-        .pipe(gulp.dest(dests.images));
-});
+gulp.task(`images`, () => gulp.src(paths.images)
+    .pipe(imagemin({
+        optimizationLevel: 5
+    }))
+    .pipe(gulp.dest(dests.images)));
 
-gulp.task('minify-css', () => {
-    return gulp.src(paths.styles)
-        .pipe(cleanCSS())
-        .pipe(rename('style.min.css'))
-        .pipe(gulp.dest(dests.styles));
-});
+gulp.task(`minify-css`, () => gulp.src(paths.styles)
+    .pipe(cleanCSS())
+    .pipe(rename(`style.min.css`))
+    .pipe(gulp.dest(dests.styles)));
 
-gulp.task('minify-js', () => {
-    return gulp.src(paths.scripts)
-        .pipe(minify({
-            ext: {
-                min: '.min.js'
-            },
-            noSource: '*',
-            exclude: ['mainFunc.js']
-        }))
-        .pipe(concat('all.min.js'))
-        .pipe(gulp.dest(dests.scripts));
-});
+gulp.task(`minify-js`, () => gulp.src(paths.scripts)
+    .pipe(minify({
+        ext: {
+            min: `.min.js`
+        },
+        noSource: `*`,
+        exclude: [`mainFunc.js`]
+    }))
+    .pipe(concat(`all.min.js`))
+    .pipe(gulp.dest(dests.scripts)));
 
-gulp.task('docs', () => {
-    return gulp.src(paths.docs)
-        .pipe(gulp.dest(dests.docs));
-})
+gulp.task(`docs`, () => gulp.src(paths.docs)
+    .pipe(gulp.dest(dests.docs)));
 
-gulp.task('data', () => {
-    return gulp.src(paths.data)
-        .pipe(gulp.dest(dests.data));
-})
+gulp.task(`data`, () => gulp.src(paths.data)
+    .pipe(gulp.dest(dests.data)));
